12594647bce3dd3adc2f698f120a1b8c896e6066
moustafa elsayed <moelsayed949@gmail.com>
2024-04-08 17:45:35 +0200

    work on task

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,22 +2,56 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+const navLinks = [
+  {
+    title: "Home",
+    path: "#Home",
+  },
+  {
+    title: "Blog",
+    path: "#Blog",
+  },
+  {
+    title: "Gifts",
+    path: "#Gifts",
+  },
+];
+
 const Header = () => {
   return (
     <nav className="bg-white w-full h-[68px] p-4">
-      <div className=" mx-auto">
+      <div className=" mx-auto flex flex-row justify-between items-center">
         <div className="flex justify-between items-center">
           {/* Logo */}
-          <div className="flex items-center">
-            <a href="#" className="text-black text-xl font-semibold">
-              Logo
-            </a>
+          <div className="flex flex-row justify-center items-center gap-3">
+            <div className="flex items-center">
+              <Link href={"/"}>
+                <Image
+                  src={"https://svgshare.com/i/158f.svg"}
+                  alt="logo"
+                  className="md:w-20 w-9 text-black drak:text-white"
+                  width={108}
+                  height={34}
+                />
+              </Link>
+            </div>
+            <ul className="flex flex-row gap-5 items-center">
+              {navLinks.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <Link
+                      href={item.path}
+                      className={`block py-2 pl-3 pr-4 dark:text-black text-black sm:text-xl rounded md:p-0 hover:text-blue-400 dark:hover:text-yellow-400 font-semibold`}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
 
-          {/* Navigation Links  */}
-          <div className="hidden md:block"></div>
-
-          {/* Mobile Menu Button (Hamburger)  */}
+          {/* Mobile Menu Button (Hamburger) */}
           <div className="md:hidden">
             <button className="text-white focus:outline-none">
               <svg
@@ -28,13 +62,61 @@ const Header = () => {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16m-7 6h7"
                 ></path>
               </svg>
             </button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-row justify-center items-center gap-3">
+            <div className="flex items-center">
+              <div className="border-r border-yellow-500 ">
+                <Link href={"/"}>
+                  <Image
+                    src={"https://svgshare.com/i/159j.svg"}
+                    alt="logo"
+                    className=" text-black drak:text-white"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+              </div>
+              <div className="border-r border-yellow-500 ">
+                {" "}
+                <Link href={"/"}>
+                  <Image
+                    src={"https://svgshare.com/i/15B5.svg"}
+                    alt="logo"
+                    className=" text-black drak:text-white"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+              </div>
+
+              <div className="border-r border-yellow-500 ">
+                {" "}
+                <Link href={"/"}>
+                  <Image
+                    src={"https://svgshare.com/i/15BS.svg"}
+                    alt="logo"
+                    className=" text-black drak:text-white"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+              </div>
+
+              <button>sjhjsfh</button>
+              <div>
+                <div>icon</div>
+                <div>icon</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
