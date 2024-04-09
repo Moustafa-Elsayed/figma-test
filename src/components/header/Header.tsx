bdeedb4460c8b07bdb8e73b33450d8883b7780c8
moustafa elsayed <moelsayed949@gmail.com>
2024-04-09 14:13:46 +0200

    work on dropmenu MobileView

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,13 +3,15 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import NavLink from "./NavLink";
+import MenuOverlay from "./MenuOverlay";
 const Header = () => {
   const [navBarOpen, setNavBarOpen] = useState(false);
 
+
   return (
     <nav className="bg-white h-[68px] p-4 overflow-hidden">
-      <div className="max-w-7xl mx-auto  px-4 sm:px-6 ">
-        <div className=" mx-auto flex flex-row justify-between items-center px-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="mx-auto flex flex-row justify-between items-center">
           <div className="flex justify-between items-center">
             {/* Logo */}
             <div className="flex flex-row justify-center items-center gap-5">
@@ -17,7 +19,7 @@ const Header = () => {
                 <button
                   className="md:hidden block"
                   onClick={() => {
-                    setNavBarOpen(true);
+                    setNavBarOpen(!navBarOpen);
                   }}
                 >
                   <Image
@@ -36,60 +38,12 @@ const Header = () => {
                   />
                 </Link>
               </div>
-              <div className="hidden md:block">
-                <ul className="flex space-x-5">
-                  <li className="relative">
-                    <Link
-                      href="/"
-                      className="text-[#D20653] font-bold text-sub_header "
-                    >
-                      Home
-                    </Link>
-                    <div className="absolute border-b-[10px]  border-[#D20653] w-full -bottom-5 rounded-3xl"></div>
-                  </li>
-                  <li>
-                    <Link
-                      href="/about"
-                      className="text-light_Text text-sub_header font-normal"
-                    >
-                      Blog
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/services"
-                      className="text-light_Text text-sub_header "
-                    >
-                      Gifts
-                    </Link>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
-          <div className="mobile-menu block md:hidden relative">
-            {!navBarOpen ? (
-              <div className="flex flex-row justify-center items-center gap-3">
-                <button
-                  onClick={() => {
-                    setNavBarOpen(true);
-                  }}
-                  className="text-slate-500 p-3 border dark:border-white border-black rounded-md flex items-center justify-center transition-all duration-300"
-                ></button>
-              </div>
-            ) : (
-              <div className="flex flex-row justify-center items-center gap-3">
-                <button
-                  onClick={() => {
-                    setNavBarOpen(false);
-                  }}
-                  className="text-slate-500 p-3 border dark:border-white border-black rounded-md flex items-center justify-center transition-all duration-300"
-                ></button>
-              </div>
-            )}
-          </div>
+          
           <NavLink />
-        </div>
+        </div> 
+        {navBarOpen && <MenuOverlay setNavBarOpen={setNavBarOpen} navBarOpen={navBarOpen}/>}
       </div>
     </nav>
   );
--- src/components/header/MenuOverlay.tsx
+++ src/components/header/MenuOverlay.tsx
@@ -1,29 +1,67 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const MenuOverlay = () => {
+interface MenuOverlayProps {
+  setNavBarOpen: (open: boolean) => void;
+  navBarOpen: boolean;
+}
+
+const MenuOverlay: React.FC<MenuOverlayProps> = ({
+  setNavBarOpen,
+  navBarOpen,
+}) => {
   return (
-    <div className="flex flex-col py-4 items-center transition-opacity duration-300 bg-black">
-      <ul className="flex flex-col space-x-5 ">
-        <li>
-          <Link href="/" className="text-[#D20653] font-bold text-sub_header ">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/about"
-            className="text-light_Text text-sub_header font-normal"
-          >
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link href="/services" className="text-light_Text text-sub_header ">
-            Gifts
-          </Link>
-        </li>
-      </ul>
+    <div className="absolute top-16 left-0 w-full bg-white border-b border-gray-200 border-t border-t-gray-200-50  ">
+      <button
+        onClick={() => setNavBarOpen(!navBarOpen)}
+        className="absolute top-0 right-10 text-2xl "
+      >
+        X
+      </button>
+      <div className="flex flex-col justify-center items-center gap-3 mt-9">
+        
+        <ul className="flex flex-col space-y-2 px-4 py-2">
+          <li>
+            <Link href="/" className="text-[#D20653] font-bold text-sub_header">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className="text-light_Text text-sub_header font-normal"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="text-light_Text text-sub_header">
+              Gifts
+            </Link>
+          </li>
+        </ul>
+        <button className=" flex flex-row justify-evenly items-center bg-gradient-to-r from-[#D20653] to-[#FF951D] w-[172px] h-[40px] rounded-lg">
+          <Image
+            src={"https://svgshare.com/i/159k.svg"}
+            alt="logo"
+            className=" text-black drak:text-white"
+            width={24}
+            height={24}
+          />
+          <p className="text-white">Add new product</p>
+        </button>
+        <div className=" flex-row justify-center items-center gap-2 flex">
+          <Image
+            src={"https://svgshare.com/i/15AQ.svg"}
+            alt="logo"
+            className=" text-black drak:text-white border-r border-gray-500 pr-1 "
+            width={24}
+            height={24}
+          />
+          <div className="font-bold text-sub_header">EN</div>
+        </div>
+      </div>
     </div>
   );
 };
